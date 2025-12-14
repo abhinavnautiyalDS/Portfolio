6a7fa9c8c6faab691500398ab8365e36724ca2ac
Abhinav Nautiyal <164336356+abhinavnautiyalDS@users.noreply.github.com>
2025-12-14 22:29:17 +0530

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function showVideo() {
+  document.getElementById("videoIntro").classList.remove("hidden");
+  document.getElementById("writtenIntro").classList.add("hidden");
+}
+
+function showWritten() {
+  document.getElementById("writtenIntro").classList.remove("hidden");
+  document.getElementById("videoIntro").classList.add("hidden");
+}
